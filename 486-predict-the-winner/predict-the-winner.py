72fefdf53e1e3116fdd6983d12fe21201e5eed4b
Nik-coder-10 <kumkumsan567@gmail.com>
2026-08-01 23:53:08 +0530

    Time: 1567 ms (19.32%) | Memory: 19.4 MB (42.77%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.py
+++ 486-predict-the-winner/predict-the-winner.py
@@ -1,15 +1,10 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-        n = len(nums)
-        dp = [[0] * n for _ in range(n)]
+        def dfs(l, r):
+            if l == r:
+                return nums[l]
 
-        for i in range(n):
-            dp[i][i] = nums[i]
+            return max(nums[l] - dfs(l + 1, r),
+                       nums[r] - dfs(l, r - 1))
 
-        for length in range(2, n + 1):
-            for l in range(n - length + 1):
-                r = l + length - 1
-                dp[l][r] = max(nums[l] - dp[l + 1][r],
-                               nums[r] - dp[l][r - 1])
-
-        return dp[0][n - 1] >= 0
+        return dfs(0, len(nums) - 1) >= 0
